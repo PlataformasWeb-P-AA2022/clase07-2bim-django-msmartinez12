46cb13c19df558194775093d758a3b0760ccc120
msmartinez12 <msmartinez12@utpl.edu.ec>
2022-07-18 19:46:04 -0500

    cambios

---
+++ demo02/src/components/TaskItems.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="tasks_container">
+        <div class="tasks_content">
+            <h1>Listado de Numeros</h1>
+            <!--ul class="tasks_list"-->
+                <span v-for="numerost in tasks" :key="numerost.telefono">
+                    <p><b>Telefono:</b> {{ numerost.telefono }}</p>
+                    <p><b>Tipo:</b> {{ numerost.tipo }}</p>
+
+                    <hr>
+                </span>
+            <!--/ul-->
+        </div>
+    </div>
+  <hr>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                // tasks
+                tasks: [''],
+                telefono: '',
+                tipo: '',
+            }
+        },
+        methods: {
+            async getData() {
+                try {
+                    // fetch tasks
+                    const response = await this.$http.get('http://localhost:8000/api/numerost/');
+                    // set the data returned as tasks
+                    this.tasks = response.data;
+                } catch (error) {
+                    // log the error
+                    console.log("aa" + error);
+                }
+            },
+        },
+        created() {
+            // Fetch tasks on page load
+            this.getData();
+        }
+    }
+</script>
